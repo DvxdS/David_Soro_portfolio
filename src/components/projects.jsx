c1b6699042e6f44548f6b37d67c684385ba106ad
David Soro <d.soro@alustudent.com>
2024-01-22 21:24:49 +0000

    Added few modification to the page

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Tracker from '../assets/tracker.png';
 import Car from '../assets/car.png';
 import Heal from '../assets/healtech.png';
+import { FaGithub } from 'react-icons/fa';
 const projects = () => {
   return (
     <div class="container my-24 mx-auto md:px-6" id="Projects">
@@ -23,10 +24,14 @@ const projects = () => {
         </div>
         <h5 class="mb-4 text-lg font-bold text-orange-400">Expenses Tracker</h5>
         <div class="mb-4 flex items-center justify-center text-sm font-medium text-danger dark:text-danger-500 lg:justify-start">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 mr-2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
-          </svg>
+        <div class="shrink-0">
+                <div class="">
+                
+               
+                  <FaGithub className="h-6 w-6 text-black" />
+               
+              </div>
+                </div>
           Hot news
         </div>
         <p class="text-neutral-500 dark:text-neutral-300">
